8171256792598e6fce840de80cff997c8a82978e
Michael <junhyeok.hong@mail.utoronto.ca>
2021-01-08 12:12:28 -0500

    Initial Commit

---
+++ Banking Record System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Banking Record System
+//
+//  Created by Michael Hong on 2021-01-08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
