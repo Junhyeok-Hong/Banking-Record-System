eaf54a09c9b3c068e4741eb8d43605fb7cc15744
Michael <junhyeok.hong@mail.utoronto.ca>
2021-01-08 12:12:52 -0500

    Included Abstract for the Project

--- Banking Record System/main.cpp
+++ Banking Record System/main.cpp
@@ -5,10 +5,67 @@
 //  Created by Michael Hong on 2021-01-08.
 //
 
-#include <iostream>
+//
+//  main.cpp
+//  Banking Record System
+//
+//  Created by Michael Hong on 2021-01-08.
+//
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+
+class account_query
+{
+private:
+    
+public:
+    void read_data();
+    void show_data();
+    void write_rec();
+    void read_rec();
+    void search_rec();
+    void edit_rec();
+    void delete_rec();
+};
+
+void account_query::read_data()
+{
+
+}
+
+void account_query::show_data()
+{
+
+}
+
+void account_query::write_rec()
+{
+
+}
+
+void account_query::read_rec()
+{
+
+}
+
+void account_query::search_rec()
+{
+
+}
+
+void account_query::edit_rec()
+{
+
+}
+
+void account_query::delete_rec()
+{
+
+}
+
+int main()
+{
     return 0;
 }
+
+
